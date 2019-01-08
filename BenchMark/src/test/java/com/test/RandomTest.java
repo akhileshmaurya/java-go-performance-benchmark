f6a493e03839426bb59117c458853d9a7e503bbe
akhileshmaurya <maurya_akhilesh@ymail.com>
2019-01-08 21:10:01 +0530

    Fix

--- BenchMark/src/test/java/com/test/RandomTest.java
+++ BenchMark/src/test/java/com/test/RandomTest.java
@@ -1,9 +1,15 @@
 
 package com.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RandomTest {
 
-  public static void main(String[] args) {
+  private static Logger logger = LogManager.getLogger(RandomTest.class);
 
+  public static void main(String[] args) {
+    logger.info("In Main Class");
+    logger.debug(" In Main Class Debug");
   }
 }
